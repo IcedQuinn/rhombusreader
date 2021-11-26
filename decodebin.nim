5de53e238b165ca6066745ff49a94c739179e999
Iced Quinn <icedquinn@iceworks.cc>
2021-11-26 02:49:20 -0600

    feat: decoding 2-bit strings

--- decodebin.nim
+++ decodebin.nim
@@ -26,7 +26,60 @@ func normalize_hexchar(ch: char): char =
    return ch
 
 func decode2b(source: string): string =
-   discard
+   # count how many bits are in source
+   # figure out how many bits modulo a byte
+   # first byte will be cut off after remainder
+   # remaining bytes are stuffed with all eight bits
+   let valid = 0..source.high
+   var accum, here, shifts, hits = 0
+   while here in valid:
+      while here in valid and is_whitespace(source[here]): inc here
+      if here notin valid: break
+      if source[here] notin '0'..'1':
+         # TODO better exception
+         raise new_exception(Exception, "Not valid bit character.")
+      inc hits
+      inc here
+
+   here = 0
+   # count how many bits we have modulo a full byte
+   # if that is more than zero then invert the number to know how many
+   # bits we have to pretend to have already shifted
+   shifts = hits mod 8
+   if shifts > 0: shifts = 8 - shifts
+
+   while here in valid:
+      while here in valid and is_whitespace(source[here]): inc here
+      if here notin valid: break
+
+      var needle = 0
+      if source[here] == '1': needle = 1
+      inc here
+
+      accum = (accum shl 1) + needle
+      inc shifts
+
+      if shifts >= 8:
+         result.add accum.char
+         accum = 0
+         shifts = 0
+   # we already took care of the stragglers so this should always be true
+   assert shifts == 0
+
+when is_main_module:
+   block test:
+      var a = "00001111"
+      let ah = decode2b(a)
+      assert ah[0].int == 15
+
+      var b = "11111111"
+      let bh = decode2b(b)
+      assert bh[0].int == 255
+
+      var c = "111111111111"
+      let ch = decode2b(c)
+      assert ch[0].int == 15
+      assert ch[1].int == 255
 
 func decode16b*(source: string): string =
    let valid = 0..source.high
@@ -72,18 +125,19 @@ func decode16b*(source: string): string =
       result.add cast[char](builder)
 
 when is_main_module:
-   var a = "fF"
-   let ah = decode16b(a)
-   assert ah[0].int == 255
+   block test:
+      var a = "fF"
+      let ah = decode16b(a)
+      assert ah[0].int == 255
 
-   var b = "F"
-   let bh = decode16b(b)
-   assert bh[0].int == 15
+      var b = "F"
+      let bh = decode16b(b)
+      assert bh[0].int == 15
 
-   var c = "FF0F"
-   let ch = decode16b(c)
-   assert ch[0].int == 255
-   assert ch[1].int == 15
+      var c = "FF0F"
+      let ch = decode16b(c)
+      assert ch[0].int == 255
+      assert ch[1].int == 15
 
 func decode64b(source: string): string =
    discard
